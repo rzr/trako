c0919b3dd283504c170269f05b3202efbb494c5b
Philippe Coval <rzr@gna.org>
2013-07-25 22:30:02 +0200

    mutex templated by type

--- src/trako/MetaMutex.hpp
+++ src/trako/MetaMutex.hpp
@@ -5,3 +5,23 @@
  *****************************************************************************/
 #include "config.h"
 #include "MetaMutex.h"
+
+
+#ifndef CONFIG_SUPPORT_API_PTHREAD_NO
+template<typename T>
+pthread_mutex_t MetaMutex<T>::mMutex = PTHREAD_MUTEX_INITIALIZER;
+
+template<typename T>
+MetaMutex<T>::MetaMutex()
+{
+  pthread_mutex_lock(&mMutex);
+}
+
+
+template<typename T>
+MetaMutex<T>::~MetaMutex()
+{
+  pthread_mutex_unlock(&mMutex);
+}
+
+#endif
