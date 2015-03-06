b0f1070440f103babf5a1b00b73de521fcc007f9
Philippe Coval <philippe.coval@open.eurogiciel.org>
2015-03-07 00:53:47 +0100

    obsolete: moved to  from https://gitorious.org/trako/trako
    Signed-off-by: Philippe Coval <philippe.coval@open.eurogiciel.org>

--- src/trako/MetaMutex.cpp
+++
@@ -1,7 +0,0 @@
-/* #ident "$Id:$"
- * @author: Philippe Coval <rzr@gna.org> -- Rev: $Author: rzr $
- * Copyright: LGPL-2 (See README file that comes with this distribution)
- *                                                           -*- Mode: c++ -*-
- *****************************************************************************/
-#include "config.h"
-#include "MetaMutex.h"
