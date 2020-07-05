726f5d1c9bdcaf9df56c6beffc2abf1e491fa6cf
Philippe Coval <rzr@users.sf.net>
2020-07-06 00:51:47 +0200

    lib: Also disable macros
    Signed-off-by: Philippe Coval <rzr@users.sf.net>
    Change-Id: Ic105008b77e847c9b525208113a568c2efbb217a

--- src/trako/trako.h
+++ src/trako/trako.h
@@ -9,20 +9,25 @@
 namespace trako {};
 
 #ifndef CONFIG_WANT_LIBTRAKO
-#  warning "trako: not enabled maybe it can be removed :TODO:"
-#  define TRAKO(CMD)
-#  define TRAKO_CLASS(type)
-#  define TRAKO_FUNCT() //!<deprecated used TRAKO(CMD)
-#  define TRAKO_SCOPE(name)
-#  define TRAKO_DIFF()
-#  define TRAKO_COUNT()
-#  define TRAKO_TYPE(type)
-#  define TRAKO_TYPE_OF(object)
-#else
+# define CONFIG_WANT_LIBTRAKO 0
+#endif
 
+#if CONFIG_WANT_LIBTRAKO
 #  warning "trako: enabled it may slowdown your program :TODO:"
 #  include "lib.h"
+#else
+#  warning "trako: not enabled maybe it can be removed :TODO:"
+#  define TRAKO(CMD) /// !<empty block, that will not be compiled
+#  define TRAKO_1(CMD)  /// !<empty block have no effect on perf
+#  define TRAKO_2(CMD, a) ///!<empty if command use args
 
+#  define TRAKO_CLASS(type) /// !<deprecated, prefer TRAKO_2(CLASS, type)
+#  define TRAKO_FUNCT() /// !<deprecated, prefer TRAKO_1(FUNCT)
+#  define TRAKO_SCOPE(name) /// !<deprecated prefer TRAKO_2(SCOPE, name)
+#  define TRAKO_DIFF() /// !<deprecated prefer TRAKO_1(...)
+#  define TRAKO_COUNT() /// !<deprecated prefer TRAKO_N(COUNT)
+#  define TRAKO_TYPE(type) /// !<deprecated prefer TRAKO_N(TYPE, name)
+#  define TRAKO_TYPE_OF(object) /// !<deprecated prefer TRAKO_N(...)
 #endif
 
 #endif //_h_
