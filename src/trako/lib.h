7a55f9664ec091e43140d32ae284957877f1fb1f
Philippe Coval <rzr@users.sf.net>
2020-07-07 13:02:14 +0200

    Add PRINT macros and update example
    Signed-off-by: Philippe Coval <rzr@users.sf.net>
    Change-Id: I057e2f592561615558d64e731445a3e8966672fc

--- src/trako/lib.h
+++ src/trako/lib.h
@@ -17,64 +17,77 @@ namespace trako {
 #include "macros.h"
 
 /// Generic macros to minimize pollution symbols
-#  define TRAKO_ exit(42)
-
 #  define TRAKO(CMD)                            \
   TRAKO_##CMD
 
 #  define TRAKO_1(CMD)                          \
   TRAKO(CMD)()
 
-#  define TRAKO_2(CMD,a)                         \
+#  define TRAKO_2(CMD, a)                        \
   TRAKO(CMD)(a)
 
 /// macro to inject a trako's meta object into the user's class
-#  define TRAKO_CLASS(type)					\
-  private: trako::MetaClass<type> mMeta ## type ## _	/*<! TrakO */	\
+#  define TRAKO_CLASS(type)                                             \
+  private: trako::MetaClass<type> mTrakoMetaClass_      /*<! TrakO */   \
   //}
 
 /// macro to inject a trako's meta object into the user's function
-#  define TRAKO_FUNCT_1(verbose)						\
+#  define TRAKO_FUNCT_1(verbose)                                        \
   trako::MetaClass<> lTrakoMetaFunct_(TRAKO_TAG("FUNCT: "), PRETTY_FUNCTION, verbose)  /*<! TrakO */ \
   //}
-#  define TRAKO_FUNCT()	\
+#  define TRAKO_FUNCT_VERBOSE()                 \
   TRAKO_FUNCT_1(true)
 
-/// macro to inject a trako's meta object into user's scope 
+#  define TRAKO_FUNCT_SILENT()                  \
+  TRAKO_FUNCT_1(false)
+
+#  define TRAKO_FUNCT()                         \
+  TRAKO_FUNCT_VERBOSE()
+
+/// macro to inject a trako's meta object into user's scope
 /// @param name : cstring to locate context
-#  define TRAKO_SCOPE(name)				\
-  trako::MetaClass<> lTrakoMetaScope_(TRAKO_TAG("SCOPE: "), name "@" FILE_LINE)  /*<! TrakO */ 
-  //}
+#  define TRAKO_SCOPE(name)                                             \
+  trako::MetaClass<> lTrakoMetaScope_(TRAKO_TAG("SCOPE: "), name "@" FILE_LINE)  /*<! TrakO */
+//}
 
 // ### tracing helper ###
 
 #  define TRAKO_CALL_TAG_(ns, funct)            \
   ns::funct(TRAKO_TAG(#funct ": ") )
 
-#  define TRAKO_CONTEXT(funct)                  \
+#  define TRAKO_CONTEXT(funct)                                  \
   TRAKO_CALL_TAG_(trako::Context<>, getInstance().funct)
 
 /// trace all stats of all tracked class
-#  define TRAKO_COUNT()			\
+#  define TRAKO_COUNT()                         \
   TRAKO_CONTEXT(printStats)
 
 /// trace changed stats of all tracked class since previous trace
-#  define TRAKO_DIFF()			\
+#  define TRAKO_DIFF()                          \
   TRAKO_CONTEXT(printDiff)
 
+#  define TRAKO_PRINT_FUNCT()                   \
+  TRAKO_CONTEXT(printDurationStats)
+
+#  define TRAKO_PRINT_CLASS()                   \
+  TRAKO_CONTEXT(printStats)
+
+#  define TRAKO_PRINT(type)                     \
+  TRAKO_PRINT#type
+
 /// trace stats of the tracked class from tracked typename
-#  define TRAKO_TYPE( type )			\
+#  define TRAKO_TYPE( type )                                    \
   trako::MetaClass<type>::printStats(TRAKO_TAG("type: "), true)
 
 /// trace stats of the tracked class from tracked object
 #if 0
-#  define TRAKO_TYPE_OF( object )		\
+#  define TRAKO_TYPE_OF( object )               \
   TRAKO_TYPE( typeof(object) )
 #else
-#  define TRAKO_TYPE_OF( object )		\
+#  define TRAKO_TYPE_OF( object )               \
   TRAKO_TYPE( void*)
 #endif
 
-  
+
 
 #endif
